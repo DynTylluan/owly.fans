5f9096caae2d6a28fc1a6389a8e62622286d34c4
Cass "Owly" Python <owlman@protonmail.com>
2021-09-06 16:38:27 +0100

    Site as of 2021-09-05

---
+++ script.js
@@ -0,0 +1,5 @@
+var pick=~~(Math.random()*359),
+    tag=document.createElement('style'),
+    style='::-moz-selection {color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::-webkit-selection{color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::selection{color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}';
+  tag.innerHTML=style.replace(/\$pick/g,pick);
+  document.body.appendChild(tag);
