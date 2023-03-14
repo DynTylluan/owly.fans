ffc5ec365d9c5900ef207209493e37bf9c24d6ab
Cass "Owly" Python <52046695+DynTylluan@users.noreply.github.com>
2023-03-14 03:55:56 +0000

    New license plus other updates

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Thank you to vantablack of vantaa.black for letting me use their code
+// Please see /humans.txt for more credits
+
 var pick=~~(Math.random()*359),
     tag=document.createElement('style'),
     style='::-moz-selection {color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::-webkit-selection{color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::selection{color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}';
