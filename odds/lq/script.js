3bcce3d3d536ff9feefffb5a2413222767e2178d
Cass <owlman@protonmail.com>
2023-12-09 04:26:02 +0000

    Example for a LibreQuake website's Christmas theme

--- odds/lq/script.js
+++ odds/lq/script.js
@@ -3,3 +3,9 @@ var pick=~~(Math.random()*359),
     style='::-moz-selection {color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::-webkit-selection{color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::selection{color:black;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}';
   tag.innerHTML=style.replace(/\$pick/g,pick);
   document.body.appendChild(tag);
+
+function playAudio(audioFile) {
+  let audioElement = document.getElementById('audioElement');
+  audioElement.src = audioFile;
+  audioElement.play();
+}
